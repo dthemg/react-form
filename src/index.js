7c335d618ab1699b6a1d465d83245b36e47d9344
dthemg <david.montgomery@live.se>
2020-03-15 12:29:51 +0100

    preparing for database

--- src/index.js
+++ src/index.js
@@ -11,9 +11,10 @@ class FormInput extends React.Component {
                     <label>
                         Name
                         <input 
-                            type="text" 
+                            type="text"
+                            name="nameValue"
                             value={this.props.nameValue} 
-                            onChange={(event) => this.props.onNameChange(event)} 
+                            onChange={(event) => this.props.onChange(event)} 
                         />
                     </label>
                 </form>
@@ -25,19 +26,39 @@ class FormInput extends React.Component {
         return (
             <div>
                 <textarea
+                    type="text"
+                    name="textAreaValue"
                     value={this.props.textAreaValue}
-                    onChange={(event) => this.props.onTextAreaChange(event)}
+                    onChange={(event) => this.props.onChange(event)}
                 />
            </div>
         )
     }
 
     renderSubmitButton() {
-        return(
+        return (
             <div>
                 <button onClick={this.props.onSubmit}>
                     Submit
                 </button>
+            </div>
+        )
+    }
+
+    renderSelectOptions() {
+        return (
+            <div>
+                <select 
+                    onChange={(event) => this.props.onChange(event)}
+                    name="selectedOption"
+                >
+                    <option value="option1">Option1</option>
+                    <option value="option2">Option2</option>
+                    <option value="option3">Option3</option>
+                    <option value="option4">Option4</option>
+                    <option value="option5">Option5</option>
+                    <option value="option6">Option6</option>
+                </select>
             </div>
         )
     }
@@ -47,6 +68,7 @@ class FormInput extends React.Component {
             <div>
                 { this.renderNameInput() }
                 { this.renderTextArea() }
+                { this.renderSelectOptions() }
                 { this.renderSubmitButton() }
             </div>
         )
@@ -57,40 +79,36 @@ class MyFormHandler extends React.Component {
     constructor(props) {
         super(props);
         
-        this.onNameChange = this.onNameChange.bind(this);
-        this.onTextAreaChange = this.onTextAreaChange.bind(this);
+        this.onChange = this.onChange.bind(this);
         this.onSubmit = this.onSubmit.bind(this);
 
         this.state = {
             nameValue: "",
             textAreaValue: "",
+            selectedOption: "",
         }
     }
 
-    onNameChange(event) {
-        this.setState({ formValue: event.target.value });
-        console.log(this.state.formValue)
-    }
-
-    onTextAreaChange(event) {
-        this.setState({ textAreaValue: event.target.value })
-        console.log(this.state.textAreaValue)
+    onChange(event) {
+        const name = event.target.name
+        const value = event.target.value;
+        this.setState({ [name]: value })
     }
 
     onSubmit() {
         console.log("Adding to database...");
+        console.log(this.state)
     }
-
 
     render() {
         return (
             <div>
                 <FormInput 
-                    onNameChange = { this.onNameChange }
+                    onChange = { this.onChange }
                     onSubmit = { this.onSubmit }
                     formValue = { this.state.nameValue }
                     textAreaValue = { this.state.textAreaValue }
-                    onTextAreaChange = { this.onTextAreaChange }
+                    
                 />
             </div>
         )
